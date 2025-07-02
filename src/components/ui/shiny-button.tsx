6a634137f4f31511a75dc92808557bd1fdecef57
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-07-02 18:28:21 +1000

    tailwind updates

--- src/components/ui/shiny-button.tsx
+++ src/components/ui/shiny-button.tsx
@@ -3,31 +3,31 @@
 import React from "react";
 import {
   motion,
-  type AnimationProps,
+  // type AnimationProps,
   type HTMLMotionProps,
 } from "framer-motion";
 import { cn } from "@/lib/utils";
 
-const animationProps = {
-  initial: { "--x": "100%", scale: 0.8 },
-  animate: { "--x": "-100%", scale: 1 },
-  whileTap: { scale: 0.95 },
-  transition: {
-    repeat: Infinity,
-    repeatType: "loop",
-    repeatDelay: 1,
-    type: "spring",
-    stiffness: 20,
-    damping: 15,
-    mass: 2,
-    scale: {
-      type: "spring",
-      stiffness: 200,
-      damping: 5,
-      mass: 0.5,
-    },
-  },
-} as AnimationProps;
+// const animationProps = {
+//   initial: { "--x": "100%", scale: 0.8 },
+//   animate: { "--x": "-100%", scale: 1 },
+//   whileTap: { scale: 0.95 },
+//   transition: {
+//     repeat: Infinity,
+//     repeatType: "loop",
+//     repeatDelay: 1,
+//     type: "spring",
+//     stiffness: 20,
+//     damping: 15,
+//     mass: 2,
+//     scale: {
+//       type: "spring",
+//       stiffness: 200,
+//       damping: 5,
+//       mass: 0.5,
+//     },
+//   },
+// } as AnimationProps;
 
 interface ShinyButtonProps extends HTMLMotionProps<"button"> {
   children: React.ReactNode;
@@ -43,7 +43,7 @@ const ShinyButton = React.forwardRef<HTMLButtonElement, ShinyButtonProps>(
     return (
       <motion.button
         ref={ref}
-        {...animationProps}
+        // {...animationProps}
         {...props}
         className={cn(
           "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
